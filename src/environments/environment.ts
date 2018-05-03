f33d7d3cd45c70e98f5775dd56aed0c51d9529f8
Bituku Praveen <bitukupraveen@gmail.com>
2018-05-03 12:19:57 +0530

    sdf

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBVjcLd28x1GwXtvHRqztY19tStTOdxKX0",
+        authDomain: "newapp-4ffe1.firebaseapp.com",
+        databaseURL: "https://newapp-4ffe1.firebaseio.com",
+        projectId: "newapp-4ffe1",
+        storageBucket: "newapp-4ffe1.appspot.com",
+        messagingSenderId: "290051612177"
+    
+    }
+};

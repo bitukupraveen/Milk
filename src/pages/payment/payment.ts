dea605756e6587469d19fbbabee49041c5a11154
Bituku Praveen <bitukupraveen@gmail.com>
2018-05-29 17:03:33 +0530

    added payment page

---
+++ src/pages/payment/payment.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2/database';
+import { Subscription } from 'rxjs/Subscription';
+import { Coustomer } from '../../models/coustomer/coustomer.interface';
+import { Payment } from '../../models/payment/payment.interface';
+
+/**
+ * Generated class for the PaymentPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-payment',
+  templateUrl: 'payment.html',
+})
+export class PaymentPage {
+  payment = {} as Payment;
+  coustomer = {} as Coustomer;
+  coustomerSubscription: Subscription;
+  coustomerRef$: FirebaseObjectObservable<Coustomer>;
+  payRef$: FirebaseObjectObservable<Payment>;
+  paymentRef$ : FirebaseListObservable<Payment[]>;
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private database: AngularFireDatabase
+  ) {
+
+    const coustomerId = this.navParams.get('coustomerId');
+    this.coustomerRef$ = this.database.object(`coustomers/${coustomerId}`);
+    this.coustomerSubscription = this.coustomerRef$.subscribe(coustomer => {
+      this.coustomer = coustomer;
+    });
+   
+    this.paymentRef$ = this.database.list('payment');
+  
+    
+    
+  }
+  
+  savePayment(payment: Payment) {
+    var currentDate = new Date()
+    var day = currentDate.getDate()
+    var month = currentDate.getMonth() + 1
+    var year = currentDate.getFullYear()
+    var myDate = year + "-" + month + "-" + day
+
+    const coustomerId = this.navParams.get('coustomerId');
+    const promise =  this.paymentRef$.push({
+        coustomerId: coustomerId,
+    paymentDate:  myDate,
+    money :Number(payment.money),
+    paymentStatus :  "Add"
+    });
+    promise
+        .then(_ => {
+            console.log('Added payment');
+            this.payment = {} as Payment;
+            this.navCtrl.pop();
+        } )
+        .catch(err => console.log(err, 'Error Adding Item'));
+
+
+
+}
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PaymentPage');
+  }
+
+}
